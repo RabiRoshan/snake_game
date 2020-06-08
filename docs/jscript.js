23f9b208c8ad0cdb6b524655d07eadffd4fcf1a1
Rabi Roshan <rabiroshankottampara@gmail.com>
2020-06-08 15:29:53 +0530

    Added on screen buttons for mobile

--- docs/jscript.js
+++ docs/jscript.js
@@ -148,3 +148,14 @@ var moveSnakeInterval = setInterval(moveSnake, 80);
 
 // Call change direction function on keyboard key-down event:
 addEventListener("keydown", e => changeDirection(e.keyCode));
+
+// ON SCREEN BUTTON FOR MOBILE:
+const leftButton = document.getElementById("leftButton");
+const rightButton = document.getElementById("rightButton");
+const upButton = document.getElementById("upButton");
+const downButton = document.getElementById("downButton");
+
+leftButton.onclick = () => changeDirection(LEFT_DIR);
+rightButton.onclick = () => changeDirection(RIGHT_DIR);
+upButton.onclick = () => changeDirection(UP_DIR);
+downButton.onclick = () => changeDirection(DOWN_DIR);
